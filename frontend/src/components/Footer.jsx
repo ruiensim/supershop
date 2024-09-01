5b796175e1ea683ae10aef19f0cec9deda301242
[sim] <88473723+ruiensim@users.noreply.github.com>
2024-09-01 16:40:01 +0800

    update

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
+
+function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                <p>Super Shop &copy; {currentYear}</p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
