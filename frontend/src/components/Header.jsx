d9b63c990c4891e23253ad3d6ea4b117cb2bfa06
[sim] <88473723+ruiensim@users.noreply.github.com>
2024-09-19 13:19:47 +0800

    update for react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
-import {Navbar,Nav,Container} from 'react-bootstrap'
-import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import { Navbar,Nav,Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 import React from 'react'
@@ -9,14 +10,21 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
+                <LinkContainer to="">
                 <Navbar.Brand>
                     <img src={logo} alt="logo"/>
-                    SuperShop</Navbar.Brand>
+                    SuperShop
+                </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id = "basic-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href=""><FaShoppingCart/>Cart</Nav.Link>
-                        <Nav.Link href=""><FaUser/>Sign In</Nav.Link>
+                        <LinkContainer to="">
+                        <Nav.Link ><FaShoppingCart/>Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="">
+                        <Nav.Link ><FaUser/>Sign In</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
